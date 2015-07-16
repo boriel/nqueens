8142438fc45b852faf1a0fdfea9163eaa9599b14
Jose Rodriguez <boriel@gmail.com>
2015-07-16 17:46:40 +0200

    module diagonalMixIn renamed to diagonalmixin

--- piece/bishop.py
+++ piece/bishop.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .diagonalMixIn import DiagonalMixIn
+from .diagonalmixin import DiagonalMixIn
 
 class Bishop(DiagonalMixIn):
     """ Implements the Bishop class

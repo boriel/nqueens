a6c59c7151fe67257888b5ffcd85693abf92a8f0
Jose Rodriguez <boriel@gmail.com>
2015-07-17 17:52:20 +0200

    Encapsulates  main iterator in a class.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from board import Board, Empty
-from piece import King, Queen, Rook, Bishop, Knight
+from piece import King, Queen, Rook, Bishop, Knight, Piece
 from position import Position
 
 M, N = 3, 3
@@ -39,32 +39,47 @@ class PositionSequencer:
         return current
 
 
-def place_pieces(board, pieces, row=0, col=0):
-    occupied = {piece.position for piece in board.pieces}
+class ValidBoards:
+    """ This class implements an iterator over all valid board configurations.
+    """
+    def __init__(self, board, piece_types):
+        """
+        :param board: A board object. E.g. Board(5, 5)
+        :param piece_types: An iterable of Piece classes (e.g. [King, King, Rook])
+        """
+        assert isinstance(board, Board), "board must be a Board instance"
+        assert all(issubclass(piece_type, Piece) for piece_type in piece_types)
+        self.board = board
+        self.piece_types = piece_types
 
-    for pos in PositionSequencer(board, row, col):
-        if board.at(pos) is not Empty or pos in board.attacked:
-            continue
+    def __iter__(self):
+        yield from self._valid_boards(self.board, self.piece_types, 0, 0)
 
-        board_ = board.copy()
-        board_.place(pieces[0], pos)
+    def _valid_boards(self, board, pieces, row, col):
+        occupied = {piece.position for piece in board.pieces}
 
-        if occupied.intersection(board_.at(pos).attacked_positions):
-            continue  # not valid. The new piece is attacking previous ones
+        for pos in PositionSequencer(board, row, col):
+            if board.at(pos) is not Empty or pos in board.attacked:
+                continue
 
-        if len(pieces) == 1:
-            yield board_  # This is a solution
-        else:
-            if pieces[0] is pieces[1]:  # The same type of piece?
-                next_row, next_col = pos.row, pos.col
+            board_ = board.copy()
+            board_.place(pieces[0], pos)
+
+            if occupied.intersection(board_.at(pos).attacked_positions):
+                continue  # not valid. The new piece is attacking previous ones
+
+            if len(pieces) == 1:
+                yield board_  # This is a solution
             else:
-                next_row, next_col = 0, 0
-
-            yield from place_pieces(board_, pieces[1:], next_row, next_col)
+                if pieces[0] is pieces[1]:  # The same type of piece?
+                    next_row, next_col = pos.row, pos.col
+                else:
+                    next_row, next_col = 0, 0
+                yield from self._valid_boards(board_, pieces[1:], next_row, next_col)
 
 
 def main(*args, **kwargs):
-    for board in place_pieces(Board(M, N), [King, King, Rook]):
+    for board in ValidBoards(Board(M, N), [King, King, Rook]):
         print(board)
 
 
